742538050aa71ced26cc856972d55082dd8292b6
thaimeo03 <thaitran15072003@gmail.com>
2024-08-18 09:21:36 +0700

    Update: using shadcn/ui substitute for antd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { AntdRegistry } from '@ant-design/nextjs-registry'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <AntdRegistry>{children}</AntdRegistry>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
