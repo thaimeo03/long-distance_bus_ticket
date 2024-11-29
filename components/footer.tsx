ffdfae03cdc08f90774ca5992ad5c8acead23e4a
thaimeo03 <thaitran15072003@gmail.com>
2024-11-29 15:31:58 +0700

    Fix: truc :>

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='text-center mt-20 bg-neutral-200 border-t border-t-gray-400 py-4 text-sm'>
-      ©{new Date().getFullYear()} Created by Thai meo
+      ©{new Date().getFullYear()} Created by Truc Cute
     </footer>
   )
 }
